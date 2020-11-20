a7514ab7db97879c527b79d7317875eac8bbec4e
iamovsyanka <anakstkv@gmail.com>
2020-11-20 12:24:51 +0300

    feat: implement userdto

--- src/main/java/by/ovsyanka/mylist/Dto/UserDto.java
+++ src/main/java/by/ovsyanka/mylist/Dto/UserDto.java
@@ -1,4 +1,41 @@
 package by.ovsyanka.mylist.Dto;
 
+import by.ovsyanka.mylist.Entity.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.*;
+
+@Getter
+@Setter
 public class UserDto {
+
+    @Null //автогенерация в БД
+    private Long userId;
+
+    @NotNull
+    @NotBlank(message = "Please, enter the userName!")
+    @Size(min = 4, max = 20, message = "Username must be between 4 and 20 symbols")
+    @Pattern(regexp = "^[A-z\\d*]{4,20}$")
+    private String userName;
+
+    @NotNull
+    @NotBlank(message = "Please, enter the password!")
+    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 symbols")
+    private String password;
+
+    @NotBlank(message = "Please, enter the repeat password!")
+    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 symbols")
+    private String repeatPassword;
+
+    @Email(message = "Wrong email address")
+    private String email;
+
+    public static UserDto fromUser(User user) {
+        UserDto userDto = new UserDto();
+        userDto.setUserId(user.getUserId());
+        userDto.setUserName(user.getUserName());
+
+        return userDto;
+    }
 }
