0c3d00836302d98e1a0a20751a0a19008dd20133
iamovsyanka <anakstkv@gmail.com>
2020-11-28 23:14:47 +0300

    feat: add taskconroller

--- src/main/java/by/ovsyanka/mylist/Rest/TaskRestController.java
+++ src/main/java/by/ovsyanka/mylist/Rest/TaskRestController.java
@@ -1,11 +1,31 @@
 package by.ovsyanka.mylist.Rest;
 
+import by.ovsyanka.mylist.Entity.Task;
+import by.ovsyanka.mylist.Repository.TaskRepository;
+import by.ovsyanka.mylist.Service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/api/tasks")
 public class TaskRestController {
+
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskRestController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> personList() {
+        return taskService.findAll();
+    }
+
 }
