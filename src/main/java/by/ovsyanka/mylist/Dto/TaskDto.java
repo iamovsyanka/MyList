a89dfe9758a0ff4b7a7c5854586d429ab3d6ac46
iamovsyanka <anakstkv@gmail.com>
2020-11-28 22:46:43 +0300

    feat: add taskdto

--- src/main/java/by/ovsyanka/mylist/Dto/TaskDto.java
+++ src/main/java/by/ovsyanka/mylist/Dto/TaskDto.java
@@ -1,4 +1,19 @@
 package by.ovsyanka.mylist.Dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Null;
+import java.util.Date;
+
+@Getter
+@Setter
 public class TaskDto {
+    @Null
+    private Long taskId;
+
+    private String name;
+    private String description;
+    private Date dateOfCreation;
+    private Date dateOfDeadline;
 }
