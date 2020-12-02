433961328a44515a3b201ce7442d7a45feeb3cfb
iamovsyanka <anakstkv@gmail.com>
2020-12-02 22:08:07 +0300

    feat: implement taskrepository

--- src/main/java/by/ovsyanka/mylist/Entity/Status.java
+++
@@ -1,5 +0,0 @@
-package by.ovsyanka.mylist.Entity;
-
-public enum Status {
-    ACTIVE, NOT_ACTIVE, DELETED
-}
--- src/main/java/by/ovsyanka/mylist/Repository/TaskRepository.java
+++ src/main/java/by/ovsyanka/mylist/Repository/TaskRepository.java
@@ -4,9 +4,10 @@ import by.ovsyanka.mylist.Entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    Task getByName(String name);
-
     Task findByName(String name);
+    List<Task> findAllByUserId(Long id);
 }
