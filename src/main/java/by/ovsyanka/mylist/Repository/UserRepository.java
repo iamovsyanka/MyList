63797e2527dec1a70ba17a664810bbc4052e3d2c
iamovsyanka <anakstkv@gmail.com>
2020-12-03 01:20:25 +0300

    feat: add methods

--- src/main/java/by/ovsyanka/mylist/Repository/UserRepository.java
+++ src/main/java/by/ovsyanka/mylist/Repository/UserRepository.java
@@ -4,7 +4,10 @@ import by.ovsyanka.mylist.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByName(String name);
+    Optional<User> findById(Long Id);
 }
