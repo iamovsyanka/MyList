453729d1352bd0687d023287b7f89bc03bb2a3af
iamovsyanka <anakstkv@gmail.com>
2020-12-03 01:19:44 +0300

    feat: add methods

--- src/main/java/by/ovsyanka/mylist/Repository/TaskRepository.java
+++ src/main/java/by/ovsyanka/mylist/Repository/TaskRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findAllByUserId(Long id);
     Task findByName(String name);
     void deleteTaskById(Long id);
+    Optional<Task> findById(Long id);
 }
