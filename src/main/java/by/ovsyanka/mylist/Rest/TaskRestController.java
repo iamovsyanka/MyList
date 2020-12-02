1e8479e25f3326e5412e89e94222ba36a0c91e13
iamovsyanka <anakstkv@gmail.com>
2020-12-02 22:12:14 +0300

    feat: implement get all tasks by user

--- src/main/java/by/ovsyanka/mylist/Rest/TaskRestController.java
+++ src/main/java/by/ovsyanka/mylist/Rest/TaskRestController.java
@@ -1,31 +1,46 @@
 package by.ovsyanka.mylist.Rest;
 
+import by.ovsyanka.mylist.Dto.TaskDto;
 import by.ovsyanka.mylist.Entity.Task;
-import by.ovsyanka.mylist.Repository.TaskRepository;
+import by.ovsyanka.mylist.Logging.Loggable;
 import by.ovsyanka.mylist.Service.TaskService;
-import org.springframework.beans.factory.annotation.Autowired;
+import by.ovsyanka.mylist.Service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
-@RestController
-@RequestMapping(value = "/api/tasks")
+@Controller
+@RequestMapping(value = "/api/tasks/")
 public class TaskRestController {
 
     private final TaskService taskService;
 
-    @Autowired
-    public TaskRestController(TaskService taskService) {
+    private final UserService userService;
+
+    public TaskRestController(TaskService taskService, UserService userService) {
         this.taskService = taskService;
+        this.userService = userService;
     }
 
-    @GetMapping
-    public List<Task> personList() {
-        return taskService.findAll();
+    @Loggable
+    @GetMapping(value = "list")
+    public ResponseEntity<List<TaskDto>> getTasks(Principal principal) {
+        List<Task> tasks = taskService.findAllByUserId(userService.findByName(principal.getName()).getId());
+        List<TaskDto> result = new ArrayList<>();
+
+        for (Task task : tasks) {
+            result.add(TaskDto.fromTask(task));
+        }
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 }
