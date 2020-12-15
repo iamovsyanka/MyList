e1cf6ce7c076ad74c42d9df7b6596aacd89ed9c8
iamovsyanka <anakstkv@gmail.com>
2020-12-16 02:12:05 +0300

    refactor: change endpoint

--- src/main/java/by/ovsyanka/mylist/Config/SecurityConfig.java
+++ src/main/java/by/ovsyanka/mylist/Config/SecurityConfig.java
@@ -15,13 +15,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
+    private static final String TASK_ENDPOINT = "/api/tasks/**";
     private static final String ADMIN_ENDPOINT = "/api/admin/**";
-    private static final String LOGIN_ENDPOINT = "/api/login";
-    private static final String REGISTER_ENDPOINT = "/api/register";
-    private static final String DOCS_ENDPOINT = "/mylist-api";
-    private static final String SWAGGER_ENDPOINT = "/swagger-ui.html";
 
-    @Autowired
+//    @Autowired
     public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
     }
@@ -42,12 +39,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                     .authorizeRequests()
-                    .antMatchers(LOGIN_ENDPOINT).permitAll()
-                    .antMatchers(DOCS_ENDPOINT).permitAll()
-                    //.antMatchers(SWAGGER_ENDPOINT).permitAll()
-                    .antMatchers(REGISTER_ENDPOINT).permitAll()
+                    .antMatchers(TASK_ENDPOINT).authenticated()
                     .antMatchers(ADMIN_ENDPOINT).hasRole("admin")
-                    .anyRequest().authenticated()
+                    .anyRequest().permitAll()
                 .and()
                     .apply(new JwtConfigurer(jwtTokenProvider));
     }
