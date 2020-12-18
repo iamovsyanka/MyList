5ccfda1b5ebc0a41f7588bb58c9bd63c26b7c83f
iamovsyanka <anakstkv@gmail.com>
2020-12-18 03:21:10 +0300

    feat: add api response

--- src/main/java/by/ovsyanka/mylist/Rest/AdminRestController.java
+++ src/main/java/by/ovsyanka/mylist/Rest/AdminRestController.java
@@ -6,6 +6,11 @@ import by.ovsyanka.mylist.Dto.UserDto;
 import by.ovsyanka.mylist.Entity.User;
 import by.ovsyanka.mylist.Logging.Loggable;
 import by.ovsyanka.mylist.Service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -25,6 +30,11 @@ public class AdminRestController {
 
     private final UserService userService;
 
+    @Operation(summary = "Get user by name", security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Return user",
+                    content = {@Content(mediaType = "application/json")})
+    })
     @Loggable
     @GetMapping(value = "{name}")
     public ResponseEntity<UserDto> getUserByName(@PathVariable(name = "name") String name) {
@@ -39,6 +49,11 @@ public class AdminRestController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get all users", security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Return users",
+                    content = {@Content(mediaType = "application/json")})
+    })
     @Loggable
     @GetMapping(value = "list")
     public ResponseEntity<Page<UserDto>> getUsers(Pageable pageable) {
@@ -57,6 +72,11 @@ public class AdminRestController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete user", security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Delete user",
+                    content = {@Content(mediaType = "application/json")})
+    })
     @Loggable
     @DeleteMapping(value = "{id}")
     public ResponseEntity<UserDto> deleteUser(@PathVariable("id") Long id) {
